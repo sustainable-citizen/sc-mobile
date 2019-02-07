8b4f78209cc3cb9f39228d99db7c4950af8399b7
Lindsay Elliott <lellio04@uoguelph.ca>
2019-02-06 21:36:52 -0500

    Making some progress

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -8,9 +8,35 @@ class HomeWidget extends StatelessWidget {
       appBar: AppBar(
         title: Text("Home"),
       ),
-      body: Container(
-        color: Colors.purple,
-      ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            children: <Widget>[
+              Text(
+                "Welcome to",
+                textScaleFactor: 3.0,
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                "Sustainable Citizen!",
+                textScaleFactor: 4.0,
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                "\nThank you for being a member of the Sustainable Citizen testing team!",
+                textScaleFactor: 2.0,
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                "\nCheck out your challenges to get started.Make sure you record when you complete them!",
+                textScaleFactor: 1.5,
+                textAlign: TextAlign.center,
+              )
+            ],
+          ),
+        ),
+      )
     );
   }
 }
