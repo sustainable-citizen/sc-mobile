e7add61121a5c9a3812278b79014fdfed9815a14
Lindsay Elliott <lellio04@uoguelph.ca>
2019-02-10 17:43:42 -0500

    Work in progress

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -3,8 +3,27 @@ import '../../models/user.dart';
 
 class ProfileWidget extends StatelessWidget {
   final User user;
+  bool _isLoading = false;
 
   ProfileWidget({Key key, this.user});
+
+
+  void _submit() {
+    final form = _formKey.currentState;
+
+    if (form.validate()) {
+      setState(() => _isLoading = true);
+      form.save();
+      //_service.attemptLogin(_username, _password);
+    }
+  }
+
+  // The button which is pressed to attempt login
+  final logoutButton = RaisedButton(
+    onPressed: _submit,
+    child: Text("Logout"),
+    color: Colors.green,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -12,9 +31,21 @@ class ProfileWidget extends StatelessWidget {
       appBar: AppBar(
         title: Text(user.username),
       ),
-      body: Container(
-        color: Colors.lime,
+      body: Center(
+      child: Container(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          children: <Widget>[
+            Text(
+              "\nThank you!\n",
+              textScaleFactor: 2.0,
+              textAlign: TextAlign.center,
+            ),
+            logoutButton,
+          ],
+        ),
       ),
+      )
     );
   }
 }
