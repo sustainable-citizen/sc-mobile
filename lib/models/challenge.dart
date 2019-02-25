d09e50fe070d416be1b99f8bbe4881d7de8533da
Anthony Robertson <arobertson1796@gmail.com>
2019-02-25 10:49:55 -0500

    seperating completing and active challenges as well as actually showing the proper challenge information

---
+++ lib/models/challenge.dart
@@ -0,0 +1,38 @@
+class Challenge {
+  int id;
+  String name;
+  String description;
+  DateTime startDate;
+  DateTime endDate;
+
+  Challenge(this.id, this.name, this.description, this.startDate, this.endDate);
+
+  Challenge.map(dynamic obj) {
+    this.id = obj["id"];
+    this.name = obj["name"];
+    this.description = obj["description"];
+    this.startDate = DateTime.parse(obj["start_date"]);
+    this.endDate = DateTime.parse(obj["end_date"]);
+  }
+
+  Map<String, dynamic> toMap() {
+    var map = Map<String, dynamic>();
+    map["id"] = id.toString();
+    map["name"] = name;
+    map["description"] = description;
+    map["start_date"] = startDate.toString();
+    map["end_date"] = endDate.toString();
+    return map;
+  }
+
+  @override
+  String toString() {
+    return "{ Challenge - " +
+      "id: $id, " +
+      "name: $name, " +
+      "description: $description " +
+      "startDate: $startDate " +
+      "endDate: $endDate " +
+      "}";
+  }
+}
